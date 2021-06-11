d3a196f23dc957320fe9b7fce191d22da08dc7bd
Dijana Pavlovic <dijanapavlovic@Dijanas-MacBook-Air.local>
2021-06-11 12:37:44 +0200

    format code to be more consistent with the rest

---
+++ src/pages/PreviewWithEditor/PreviewWithEditor.styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ListContainer = styled.div`
+  max-width: 976px;
+  margin: auto;
+  padding: 24px;
+`;
+
+export const Header = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+`;
+
+export const containerStyles = {
+  width: '100vw',
+  height: 'calc(100vh - 116px)',
+};
--- src/pages/PreviewWithEditor/PreviewWithEditorVirtualized.tsx
+++ src/pages/PreviewWithEditor/PreviewWithEditorVirtualized.tsx
@@ -5,44 +5,24 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Virtuoso } from 'react-virtuoso';
-import styled from 'styled-components';
 
 import { setJsonData } from 'store/actionCreators';
 import ItemPreviewWithEditor from 'components/ItemPreviewWithEditor/ItemPreviewWithEditor';
 import { formatObject } from 'shared/utils';
 import jsonData from 'examples/example-2.json';
+import { Header, ListContainer, containerStyles } from './PreviewWithEditor.styled';
 // import jsonData from 'examples/example-1.json';
-
-const ListContainer = styled.div`
-  max-width: 976px;
-  margin: auto;
-  padding: 24px;
-`;
-
-const Header = styled.div`
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
-`;
-
-const containerStyles = {
-  width: '100vw',
-  height: 'calc(100vh - 116px)',
-};
 
 const PreviewWithEditorVirtualized = (): JSX.Element => {
   const dispatch = useDispatch();
   const editorState = useSelector((state) => state);
+  const stateKeys = Object.keys(editorState);
 
   useEffect(() => {
     const data = jsonData.reduce((acc, item) => ({ ...acc, [item.id]: formatObject(item) }), {});
 
     dispatch(setJsonData(data));
   }, [dispatch]);
-
-  const stateKeys = Object.keys(editorState);
 
   return (
     <>
